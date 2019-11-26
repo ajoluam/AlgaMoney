dd083d69a9229d21dbc491157164f8929ace7fe2
ajoluam <ajoluam@hotmail.com>
2019-11-25 22:12:47 -0300

    Uso de DTO

--- algamoney-api/src/main/java/com/example/algamoney/api/resource/PessoaResource.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.algamoney.api.model.Pessoa;
+import com.example.algamoney.api.model.PessoaDTO;
 import com.example.algamoney.api.repository.PessoaRepository;
 
 @RestController
@@ -34,6 +37,16 @@ public class PessoaResource {
 	@GetMapping("/{codigo}")
 	public Pessoa buscarPeloCodigo(@PathVariable Long codigo) {
 		return pessoaRepository.findOne(codigo);
+	}
+	
+	@GetMapping("/{codigo}/DTO")
+	public PessoaDTO buscarPessoaDTO (@PathVariable Long codigo){
+		RestTemplate restTemplate = new RestTemplate();
+		String URI  = "http://localhost:8080/pessoas/1";
+		
+		PessoaDTO result = restTemplate.getForObject(URI,PessoaDTO.class);
+		
+		return result;
 	}
 	
 	@PostMapping
