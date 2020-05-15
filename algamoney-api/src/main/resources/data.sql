36aef9100cca9967fcc5fc144edb36e8c7356848
Alexandre Amorim <ajoluam@hotmail.com>
2020-05-15 18:31:18 -0300

    Acrescentar properties H2 e data.sql

---
+++ algamoney-api/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO  categoria (nome) values ('Lazer');
+INSERT INTO  categoria (nome) values ('Alimentacao');
+INSERT INTO  categoria (nome) values ('Supermercado');
+INSERT INTO  categoria (nome) values ('Farmacia');
+INSERT INTO  categoria (nome) values ('Outros');
+
+INSERT INTO pessoa (nome, ativo, logradouro, numero, complemento, bairro, cep, cidade , estado) 
+			values ('Alexandre', true, 'Av Presidente Kennedy', '1440', 'Apto 101', 'Candeias', '54430-030', 'Jaboatão', 'Pernambuco'),
+				   ('Gustavo', true, 'Rua José Moreira Leal', '284', 'Apto 102', 'Boa Viagem', '52030-380', 'Recife', 'Pernambuco'),
+				   ('Matheus Henrique', true, 'Av Presidente Kennedy', '1440', 'Apto 101', 'Candeias', '54430-030', 'Jaboatão', 'Pernambuco'),
+				   ('Ruth Maria ', true, 'Av Presidente Kennedy', '1440', 'Apto 101', 'Candeias', '54430-030', 'Jaboatão', 'Pernambuco'),
+				   ('Socorro', true, 'Rua 1 de Maio', '48', 'Casa', 'Centro', '54800-000', 'Moreno', 'Pernambuco'),
+				   ('José Paiva', true, 'Rua 1 de Maio', '48', 'Casa', 'Centro', '54800-000', 'Moreno', 'Pernambuco');
