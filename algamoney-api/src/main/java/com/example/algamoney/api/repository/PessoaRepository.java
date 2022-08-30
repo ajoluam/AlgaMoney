5bfa1fe45dfb159e4e8b7cdf1ae05825f9a71dcf
Alexandre Amorim <ajoluam@hotmail.com>
2022-08-30 20:55:15 -0300

    Ajuste

--- algamoney-api/src/main/java/com/example/algamoney/api/repository/PessoaRepository.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/repository/PessoaRepository.java
@@ -3,7 +3,13 @@ package com.example.algamoney.api.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.algamoney.api.model.Pessoa;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+
+    @Query("SELECT p FROM Pessoa p WHERE p.ativo = true")
+    List<Object> listar();
 
 }
